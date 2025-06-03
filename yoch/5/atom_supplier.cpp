4fabc0638a060c1d8c2f95555eea80e897191e19
Yocheved <yocheved@davidowitz.net>
2025-06-03 16:27:58 +0300

    yoch try

---
+++ yoch/5/atom_supplier.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <algorithm>
+
+
+constexpr size_t TCP_BUFFER_SIZE = 1024;
+
+
+std::string readLine(int fd) {
+    std::string line;
+    char c;
+    while(true) {
+        ssize_t n = recv(fd, &c, 1, 0);
+        if (n <= 0){
+            return std::string();
+        }
+        if (c == '\n') {
+            break;
+        }
+        line.push_back(c);
+    }
+    return line;
+}
+
+int main(int argc, char* argv[]) {
+    const char* host = nullptr;
+    const char* port = nullptr;
+    const char* uds_socket_path = nullptr; 
+    int opt;
+
+    while ((opt = getopt(argc, argv, "h:p:f:")) != -1) {
+        switch (opt) {
+            case 'h':
+                host = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case 'f':
+                uds_socket_path = optarg; 
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " -h <hostname/IP> -p <port>\n";
+                return 1;
+        }
+    }
+
+    bool use_network = (host && port && !uds_socket_path);
+    bool use_uds = (uds_socket_path && !host && !port);
+
+
+    if (!(use_network && use_uds)) {
+        std::cerr << "ERROR: Specify either:\n" << "-h <hostname/IP> - p <port> OR -f <uds_socket_path>\n";
+        return 1;
+    }
+
+    addrinfo hints{}, *res;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if(getaddrinfo(host, port, &hints, &res) != 0) {
+        std::cerr << "Error getting address info (TCP)" << "\n";
+        return 1;
+    }
+
+    int sock = -1;
+    for (addrinfo* p = res; p != nullptr; p = p->ai_next) {
+        sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sock < 0) {
+            continue;
+        }
+        if (connect(sock, p->ai_addr, p->ai_addrlen) == 0) {
+            break;
+        }
+        close(sock);
+        sock = -1;
+    }
+    freeaddrinfo(res);
+    if (sock < 0){
+        std::cerr << "ERROR: Unable to connect" << "\n";
+        return 1;
+    }
+    std::cout << "Connected to server" << host << "TCP port:" << port << "\n";
+    std::cout << "Type ADD CARBON|OXYGEN|HYDROGEN <number> to request atoms, or QUIT to exit\n";
+
+    std::string input;
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, input);
+        if (input == "QUIT") {
+            break;
+        }
+        if(input.back() != '\n') {
+            input.push_back('\n');
+        }
+        if(send(sock, input.c_str(), input.size(), 0) < 0) {
+            std::cerr << "Error sending data to server" << "\n";
+            break;
+        }
+
+        std::string line1 = readLine(sock);
+        if (line1.empty()) {
+            std::cerr << "Server disconnected" << "\n";
+            break;
+        }
+        std::cout << line1 << "\n";
+
+        if(line1.rfind("CARBON", 0) == 0){
+            std::string line2 = readLine(sock);
+            std::string line3 = readLine(sock);
+            if(!line2.empty()) {
+                std::cout << line2 << "\n";
+            }
+            if(!line3.empty()) {
+                std::cout << line3 << "\n";
+            }
+        }
+
+    }
+    close(sock);
+    std::cout << "Disconnected from server\n";
+    return 0;
+    }
---
+++ yoch/5/molecule_requester.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <algorithm>
+
+enum{
+    UDP_BUFFER_SIZE = 1024,
+};
+
+
+int main(int argc, char* argv[]) {
+    const char* host = nullptr;
+    const char* port = nullptr;
+    const char* uds_socket_path = nullptr;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "h:p:f:")) != -1) {
+        switch (opt) {
+            case 'h':
+                host = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case 'f':
+                uds_socket_path = optarg; 
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " -h <hostname/IP> -p <port>\n";
+                return 1;
+        }
+    }
+
+    if (host == nullptr || port == nullptr) {
+        std::cerr << "ERROR: Hostname/IP and port must be specified\n";
+        return 1;
+    }
+
+    addrinfo UDPhints{}, *resUDP;
+    UDPhints.ai_family = AF_UNSPEC;
+    UDPhints.ai_socktype = SOCK_DGRAM;
+
+    if (getaddrinfo(host, port, &UDPhints, &resUDP) != 0) {
+        std::cerr << "Error getting address info (UDP)\n";
+        return 1;
+    }
+
+    int udpsock = -1;
+    addrinfo* p;
+    for (p = resUDP; p != nullptr; p = p->ai_next) {
+        udpsock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (udpsock < 0) {
+            continue;
+        }
+        break;
+    }
+
+    if(p == nullptr) {
+        std::cerr << "Failed creating UDP socket\n";
+        freeaddrinfo(resUDP);
+        return 1;
+    }
+
+    std::cout << "Connected to server: " << host << "UDP port: " << port << "\n";
+    std::cout << "Type 'DELIVER WATER|CARBON DIOXIDE|ALCOHOL|GLUCOSE <count>', or QUIT to exit.\n";
+
+    std::string input;
+    sockaddr_storage peer;
+    socklen_t peer_len = sizeof(peer);
+
+    while (true) {
+        std::cout << "> ";
+        if(!std::getline(std::cin, input) || input == "QUIT") {
+            break;
+        }
+        if (input.empty()) {
+            continue;
+        }
+        if (input.back() != '\n') {
+            input.push_back('\n');
+        }
+
+ 
+        ssize_t sent = sendto(udpsock, input.c_str(), input.size(), 0, resUDP->ai_addr, resUDP->ai_addrlen);
+        if (sent < 0) {
+            std::cerr << "Error sending data" << "\n";
+            break;
+        }
+        char buffer[UDP_BUFFER_SIZE];
+        ssize_t n = recvfrom(udpsock, buffer, sizeof(buffer) - 1, 0, (sockaddr*)&peer, &peer_len);
+        if (n < 0) {
+            std::cerr << "Error receiving data" << "\n";
+            break;
+        }
+        buffer[n] = '\0'; // Null-terminate the received data
+        std::cout << "Received from server: " << buffer << "\n";
+
+
+    }
+
+    freeaddrinfo(resUDP);
+    close(udpsock);
+    std::cout << "Disconnected from server\n";
+    return 0;
+
+}
